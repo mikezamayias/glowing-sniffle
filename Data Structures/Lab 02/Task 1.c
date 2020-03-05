41cd1374b34bab6bd062e2a157c2f51cccdf6ef8
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-03-05 22:16:29 +0200

    Updated first task of second lab

--- Data Structures/Lab 02/Task 1.c
+++ Data Structures/Lab 02/Task 1.c
@@ -5,25 +5,60 @@
 #define Columns 5
 
 int randomIntGenerator();
+int better(int grades[][Columns]);
+void print2dArray(int grades[][Columns]);
 
 int main()
 {
     int grades[Rows][Columns];
-    int column, row;
+    int column, row, max;
 
+    //  populate grades array with random ints from 0 to 10
     for (row = 0; row < Rows; row++)
     {
         for (column = 0; column < Columns; column++)
         {
             grades[row][column] = randomIntGenerator();
-            printf("grades[%i][%i]) = %i\n", row, column, grades[row][column]);
         }
     }
+
+    max = better(grades);
+    printf("grades max:\t%i\n", max);
 
     return 0;
 }
 
 int randomIntGenerator()
 {
-    return rand() % 11;
+    return rand() % 11; //  11 as it's a unit more than 10
+}
+
+int better(int grades[][Columns])
+{
+    int rows, columns, max = grades[rows][columns];
+    rows = columns = 0;
+
+    for (columns = 1; columns < Columns; columns++)
+    {
+        for (rows; rows < Rows; rows++)
+        {
+            if (grades[rows][columns] > max)
+            {
+                max = grades[rows][columns];
+            }
+        }
+    }
+
+    return max;
+}
+
+void print2dArray(int grades[][Columns])
+{
+    for (int row = 0; row < Rows; row++)
+    {
+        for (int column = 0; column < Columns; column++)
+        {
+            printf("grades[%i][%i]) = %i\n", row, column, grades[row][column]);
+        }
+    }
 }
