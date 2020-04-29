ba07b6baf0818e3f58102cdd2c7417886a019cf0
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-04-29 19:00:39 +0300

    finding the minimum of 3 numbers works now.

--- Computer Architecture/Assignment 6/Homework/Task 1.s
+++ Computer Architecture/Assignment 6/Homework/Task 1.s
@@ -9,6 +9,9 @@
 .globl main
 main:
     jal		input_nums			# jump to input_nums and save position to $ra
+    #   temp vars on $21 and $22
+    addi	$21, $21, 0			# $21 = $21 + 0
+    addi	$22, $22, 1			# $22 = $22 + 1
     jal		min_num				# jump to min_num and save position to $ra
     j		main				# jump to main
 #    li		$v0, 10		        # $v0 = 10
@@ -41,32 +44,43 @@ input_nums:
     # return
     jr		$ra					# jump to $ra
 min_num:
-    #   check_0 on $19, ckeck_1 on $20
-    addi	$19, $19, 0			# $19 = $19 + 0
-    addi	$20, $20, 1			# $20 = $20 + 1
+    #   if there isnt min, then print min_error
+    and     $19, $16, $17
+    and     $20, $17, $18
+    beq		$19, $20, min_error	# if $19 == $20 then min_error
+    bne		$19, $20, one_comparison	# if $19 != $20 then one_comparison
+one_comparison:
+    #   if a and b are equal, then compare a and c
+    beq		$19, 1, a_c	        # if $19 == 1 then a_c
+    #   if b and c are equal, then compare a and b
+    beq		$20, 1, a_b	        # if $20 == 1 then a_b
+a_c:
+    blt		$16, $18, a_min	# if $16 < $18 then a_min
+    blt		$18, $16, c_min	# if $18 < $16 then c_min
+a_b:
+    blt		$16, $17, a_min	# if $16 < $17 then a_min
+    blt		$17, $16, b_min	# if $17 < $16 then b_min
+    #   reset $19 and $20
+    and     $19, $21, $22
+    or      $20, $21, $22
     #   check if a is the minimum, $19 and $20 must be equal
     slt     $19, $16, $17
     slt     $20, $16, $18
     beq		$19, $20, a_min	    # if $19 == $20 then a_min
-    #   check_0 on $19, ckeck_1 on $20
-    addi	$19, $19, 0			# $19 = $19 + 0
-    addi	$20, $20, 1			# $20 = $20 + 1
+    #   reset $19 and $20
+    and     $19, $21, $22
+    or      $20, $21, $22
     #   check if b is the minimum, $19 and $20 must be equal
     slt     $19, $17, $16
     slt     $20, $17, $18
     beq		$19, $20, b_min	    # if $19 == $20 then b_min
-    #   check_0 on $19, ckeck_1 on $20
-    addi	$19, $19, 0			# $19 = $19 + 0
-    addi	$20, $20, 1			# $20 = $20 + 1
+#   reset $19 and $20
+    and     $19, $21, $22
+    or      $20, $21, $22
     #   check if c is the minimum, $19 and $20 must be equal
     slt     $19, $18, $16
     slt     $20, $18, $17
     beq		$19, $20, c_min	    # if $19 == $20 then c_min
-    #   check_0 on $19, ckeck_1 on $20
-    addi	$19, $19, 0			# $19 = $19 + 0
-    addi	$20, $20, 1			# $20 = $20 + 1
-    #   if there isnt min, then print message
-    bne		$19, $20, min_error	# if $19 != $20 then min_error
 a_min:
     #   print a
     addi	$2, $0, 1			# $2 = $0 + 1
@@ -123,4 +137,3 @@ min_error:
     syscall
     # return
     jr		$ra					# jump to $ra
-
