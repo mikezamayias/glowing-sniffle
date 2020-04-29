1f264998b9c8cc420398e6584838e8535491fb54
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-04-29 19:55:39 +0300

    find the max of 3 numebrs works as well

--- Computer Architecture/Assignment 6/Homework/Task 1.s
+++ Computer Architecture/Assignment 6/Homework/Task 1.s
@@ -3,16 +3,20 @@
     int_b: .asciiz "Input integer B: "
     int_c: .asciiz "Input integer C: "
     minimum: .asciiz " is the minimum"
+    maximum: .asciiz " is the maximum"
     no_minimum: .asciiz "There is no minimum."
+    no_maximum: .asciiz "There is no maximum."
     str_nl: .asciiz "\n"
 .text
 .globl main
 main:
     jal		input_nums			# jump to input_nums and save position to $ra
-    #   temp vars on $21 and $22
+    #   vars on $21, $22 and $23
     addi	$21, $21, 0			# $21 = $21 + 0
     addi	$22, $22, 1			# $22 = $22 + 1
+    sub	    $23, $21, $22		# $23 = $21 - $22
     jal		min_num				# jump to min_num and save position to $ra
+    jal		max_num				# jump to max_num and save position to $ra
     j		main				# jump to main
 #    li		$v0, 10		        # $v0 = 10
 #    syscall
@@ -48,16 +52,16 @@ min_num:
     and     $19, $16, $17
     and     $20, $17, $18
     beq		$19, $20, min_error	# if $19 == $20 then min_error
-    bne		$19, $20, one_comparison	# if $19 != $20 then one_comparison
-one_comparison:
+    bne		$19, $20, minoc	# if $19 != $20 then minoc
+minoc:
     #   if a and b are equal, then compare a and c
-    beq		$19, 1, a_c	        # if $19 == 1 then a_c
+    beq		$19, 1, minac	        # if $19 == 1 then minac
     #   if b and c are equal, then compare a and b
-    beq		$20, 1, a_b	        # if $20 == 1 then a_b
-a_c:
+    beq		$20, 1, minab	        # if $20 == 1 then minab
+minac:
     blt		$16, $18, a_min	# if $16 < $18 then a_min
     blt		$18, $16, c_min	# if $18 < $16 then c_min
-a_b:
+minab:
     blt		$16, $17, a_min	# if $16 < $17 then a_min
     blt		$17, $16, b_min	# if $17 < $16 then b_min
     #   reset $19 and $20
@@ -74,7 +78,7 @@ a_b:
     slt     $19, $17, $16
     slt     $20, $17, $18
     beq		$19, $20, b_min	    # if $19 == $20 then b_min
-#   reset $19 and $20
+    #   reset $19 and $20
     and     $19, $21, $22
     or      $20, $21, $22
     #   check if c is the minimum, $19 and $20 must be equal
@@ -130,6 +134,100 @@ min_error:
     #   print no minimum
     addi	$2, $0, 4			# $2 = $0 + 4
     la      $4, no_minimum
+    syscall
+    #   print new line
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, str_nl
+    syscall
+    # return
+    jr		$ra					# jump to $ra
+max_num:
+    #   if there isnt max, then print max_error
+    and     $19, $16, $17
+    and     $20, $17, $18
+    beq		$19, $20, max_error	# if $19 == $20 then max_error
+    bne		$19, $20, maxoc	# if $19 != $20 then maxoc
+maxoc:
+    #   if a and b are equal, then compare a and c
+    beq		$19, 1, maxac	        # if $19 == 1 then maxac
+    #   if b and c are equal, then compare a and b
+    beq		$20, 1, maxab	        # if $20 == 1 then maxab
+maxac:
+    bgt		$16, $18, a_max	# if $16 > $18 then a_max
+    bgt		$18, $16, c_max	# if $18 > $16 then c_max
+maxab:
+    bgt		$16, $17, a_max # if $16 > $17 then a_max
+    bgt		$17, $16, b_max # if $17 > $16 then b_max
+    #   reset $19 and $20
+    and     $19, $21, $22
+    or      $20, $21, $22
+    #   check if a is the maximum, $19 and $20 must be equal
+    sgt     $19, $16, $17
+    sgt     $20, $16, $18
+    beq		$19, $20, a_min	    # if $19 == $20 then a_min
+    #   reset $19 and $20
+    and     $19, $21, $22
+    or      $20, $21, $22
+    #   check if b is the minimum, $19 and $20 must be equal
+    sgt     $19, $17, $16
+    sgt     $20, $17, $18
+    beq		$19, $20, b_min	    # if $19 == $20 then b_min
+    #   reset $19 and $20
+    and     $19, $21, $22
+    or      $20, $21, $22
+    #   check if c is the minimum, $19 and $20 must be equal
+    sgt     $19, $18, $16
+    sgt     $20, $18, $17
+    beq		$19, $20, c_min	    # if $19 == $20 then c_min
+a_max:
+    #   print a
+    addi	$2, $0, 1			# $2 = $0 + 1
+    add		$4, $0, $16		    # $4 = $0 + $16
+    syscall
+    #   print maximum
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, maximum
+    syscall
+    #   print new line
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, str_nl
+    syscall
+    # return
+    jr		$ra					# jump to $ra
+b_max:
+    #   print b
+    addi	$2, $0, 1			# $2 = $0 + 1
+    add		$4, $0, $17		    # $4 = $0 + $17
+    syscall
+    #   print maximum
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, maximum
+    syscall
+    #   print new line
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, str_nl
+    syscall
+    # return
+    jr		$ra					# jump to $ra
+c_max:
+    #   print c
+    addi	$2, $0, 1			# $2 = $0 + 1
+    add		$4, $0, $18		    # $4 = $0 + $18
+    syscall
+    #   print maximum
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, maximum
+    syscall
+    #   print new line
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, str_nl
+    syscall
+    # return
+    jr		$ra					# jump to $ra
+max_error:
+    #   print no maximum
+    addi	$2, $0, 4			# $2 = $0 + 4
+    la      $4, no_maximum
     syscall
     #   print new line
     addi	$2, $0, 4			# $2 = $0 + 4
