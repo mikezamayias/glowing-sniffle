1ced221118a0a441498a37ca74201b8fb37984f3
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-04-29 23:56:15 +0300

    it doesnt work properly

--- Computer Architecture/Assignment 6/Homework/Task 1.s
+++ Computer Architecture/Assignment 6/Homework/Task 1.s
@@ -2,19 +2,19 @@
     int_a: .asciiz "Input integer A: "
     int_b: .asciiz "Input integer B: "
     int_c: .asciiz "Input integer C: "
-    minimum: .asciiz " is the minimum"
-    maximum: .asciiz " is the maximum"
+    minimum: .asciiz " is the minimum."
+    maximum: .asciiz " is the maximum."
     no_minimum: .asciiz "There is no minimum."
     no_maximum: .asciiz "There is no maximum."
-    repeat_prompt: .asciiz "Run again or stop (use 1 or 0 respectively):"
+    repeat_prompt: .asciiz "Run again or stop, use 1 or 0 respectively: "
     str_nl: .asciiz "\n"
 .text
 .globl main
 main:
-    jal		input_nums			# jump to input_nums and save position to $ra
     #   vars on $21, $22. later used in min_num and max_num
     addi	$21, $21, 0			# $21 = $21 + 0
     addi	$22, $22, 1			# $22 = $22 + 1
+    jal		input_nums			# jump to input_nums and save position to $ra
     jal		min_num				# jump to min_num and save position to $ra
     jal		max_num				# jump to max_num and save position to $ra
 while:
@@ -26,14 +26,14 @@ while:
     addi	$2, $0, 5			# $2 = $0 + 5
     syscall
     add		$23, $2, $0         # $23 = $2 + $0
-    #   check input
-    bgt		$23, $22, while	# if $23 > $22 then while
-    blt		$23, $21, while	# if $23 < $21 then while
+    #   check
+    blt		$23, $21, while	    # if $23 < $21 then while
+    bgt		$23, $22, while	    # if $23 > $22 then while
     beq		$23, $22, exit_while	# if $23 == $22 then exit_while
     beq		$23, $21, exit_while	# if $23 == $21 then exit_while
 exit_while:
-    beq		$23, $22, main	# if $23 == $22 then main
-    beq		$23, $21, exit	# if $23 == $21 then exit
+    beq		$23, $21, exit	    # if $23 == $21 then exit
+    j		main				# jump to main
 exit:
     li		$v0, 10		        # $v0 = 10
     syscall
@@ -65,42 +65,45 @@ input_nums:
     # return
     jr		$ra					# jump to $ra
 min_num:
+#   reset $19 and $20 just in case
+    and     $19, $21, $22
+    and     $20, $22, $22
     #   if there isnt min, then print min_error
-    and     $19, $16, $17
-    and     $20, $17, $18
+    and     $19, $16, $17       # if $16 == $17 then $19 = 1
+    and     $20, $17, $18       # if $17 == $18 then $20 = 1
     beq		$19, $20, min_error	# if $19 == $20 then min_error
-    bne		$19, $20, minoc	# if $19 != $20 then minoc
+    bne		$19, $20, minoc	    # if $19 != $20 then minoc
 minoc:
     #   if a and b are equal, then compare a and c
-    beq		$19, 1, minac	        # if $19 == 1 then minac
+    beq		$19, $22, minac	    # if $19 == $22 then minac
     #   if b and c are equal, then compare a and b
-    beq		$20, 1, minab	        # if $20 == 1 then minab
+    beq		$20, $22, minab	    # if $20 == $22 then minab
 minac:
-    blt		$16, $18, a_min	# if $16 < $18 then a_min
-    blt		$18, $16, c_min	# if $18 < $16 then c_min
+    blt		$16, $18, a_min 	# if $16 < $18 then a_min
+    blt		$18, $16, c_min 	# if $18 < $16 then c_min
 minab:
-    blt		$16, $17, a_min	# if $16 < $17 then a_min
-    blt		$17, $16, b_min	# if $17 < $16 then b_min
+    blt		$16, $17, a_min	    # if $16 < $17 then a_min
+    blt		$17, $16, b_min	    # if $17 < $16 then b_min
     #   reset $19 and $20
     and     $19, $21, $22
-    or      $20, $21, $22
+    and     $20, $22, $22
     #   check if a is the minimum, $19 and $20 must be equal
-    slt     $19, $16, $17
-    slt     $20, $16, $18
+    slt     $19, $16, $17       # if $16 < $17 then $19 = 1
+    slt     $20, $16, $18       # if $16 < $18 then $20 = 1
     beq		$19, $20, a_min	    # if $19 == $20 then a_min
     #   reset $19 and $20
     and     $19, $21, $22
-    or      $20, $21, $22
+    and     $20, $22, $22
     #   check if b is the minimum, $19 and $20 must be equal
-    slt     $19, $17, $16
-    slt     $20, $17, $18
+    slt     $19, $17, $16       # if $17 < $16 then $19 = 1
+    slt     $20, $17, $18       # if $17 < $18 then $20 = 1
     beq		$19, $20, b_min	    # if $19 == $20 then b_min
     #   reset $19 and $20
     and     $19, $21, $22
-    or      $20, $21, $22
+    and     $20, $22, $22
     #   check if c is the minimum, $19 and $20 must be equal
-    slt     $19, $18, $16
-    slt     $20, $18, $17
+    slt     $19, $18, $16       # if $18 < $16 then $19 = 1
+    slt     $20, $18, $17       # if $18 < $17 then $20 = 1
     beq		$19, $20, c_min	    # if $19 == $20 then c_min
 a_min:
     #   print a
@@ -159,42 +162,45 @@ min_error:
     # return
     jr		$ra					# jump to $ra
 max_num:
+    #   reset $19 and $20 just in case
+    and     $19, $21, $22
+    and     $20, $22, $22
     #   if there isnt max, then print max_error
-    and     $19, $16, $17
-    and     $20, $17, $18
+    and     $19, $16, $17       # if $16 == $17 then $19 = 1
+    and     $20, $17, $18       # if $17 == $18 then $20 = 1
     beq		$19, $20, max_error	# if $19 == $20 then max_error
-    bne		$19, $20, maxoc	# if $19 != $20 then maxoc
+    bne		$19, $20, maxoc	    # if $19 != $20 then maxoc
 maxoc:
     #   if a and b are equal, then compare a and c
-    beq		$19, 1, maxac	        # if $19 == 1 then maxac
+    beq		$19, $22, maxac	    # if $19 == $22 then maxac
     #   if b and c are equal, then compare a and b
-    beq		$20, 1, maxab	        # if $20 == 1 then maxab
+    beq		$20, $22, maxab	    # if $20 == $22 then maxab
 maxac:
-    bgt		$16, $18, a_max	# if $16 > $18 then a_max
-    bgt		$18, $16, c_max	# if $18 > $16 then c_max
+    bgt		$16, $18, a_max	    # if $16 > $18 then a_max
+    bgt		$18, $16, c_max	    # if $18 > $16 then c_max
 maxab:
-    bgt		$16, $17, a_max # if $16 > $17 then a_max
-    bgt		$17, $16, b_max # if $17 > $16 then b_max
+    bgt		$16, $17, a_max     # if $16 > $17 then a_max
+    bgt		$17, $16, b_max     # if $17 > $16 then b_max
     #   reset $19 and $20
     and     $19, $21, $22
-    or      $20, $21, $22
+    and     $20, $22, $22
     #   check if a is the maximum, $19 and $20 must be equal
-    sgt     $19, $16, $17
-    sgt     $20, $16, $18
+    sgt     $19, $16, $17       # if $16 > $17 then $19 = 1
+    sgt     $20, $16, $18       # if $16 > $18 then $20 = 1
     beq		$19, $20, a_min	    # if $19 == $20 then a_min
     #   reset $19 and $20
     and     $19, $21, $22
-    or      $20, $21, $22
+    and     $20, $22, $22
     #   check if b is the minimum, $19 and $20 must be equal
-    sgt     $19, $17, $16
-    sgt     $20, $17, $18
+    sgt     $19, $17, $16       # if $17 > $16 then $19 = 1
+    sgt     $20, $17, $18       # if $17 > $18 then $20 = 1
     beq		$19, $20, b_min	    # if $19 == $20 then b_min
     #   reset $19 and $20
     and     $19, $21, $22
-    or      $20, $21, $22
+    and     $20, $22, $22
     #   check if c is the minimum, $19 and $20 must be equal
-    sgt     $19, $18, $16
-    sgt     $20, $18, $17
+    sgt     $19, $18, $16       # if $18 > $16 then $19 = 1
+    sgt     $20, $18, $17       # if $18 > $17 then $20 = 1
     beq		$19, $20, c_min	    # if $19 == $20 then c_min
 a_max:
     #   print a
