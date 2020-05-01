5357844233c8ef181957c1644d4cbbce620122ce
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-05-01 21:11:28 +0300

    updated task 1 code

--- Linear Algebra/Project 1/Task 1.m
+++ Linear Algebra/Project 1/Task 1.m
@@ -6,8 +6,6 @@ pkg load symbolic;
 #   set symbolic settings
 syms initpython;
 sympref display unicode;
-
-
 #   default A matrix
 A = [1, 2; 2, 1]
 #   symbolic var x
@@ -23,7 +21,6 @@ eigenvalues = double(solve(detAsubxI==0))
 #   two roots
 x1 = -1;
 x2 = 3;
-
 #   use first root
 disp("Using eigenvalue x1 = -1:")
 #   make B matrix by replacing x with -1 in A - x*I
@@ -37,7 +34,6 @@ syms k1 k2;
 k1 = solve(2*k1+2*k2-0)
 #   let k1 = 1, then k2 = -1
 Eigenvector1 = [1;-1]
-
 #   use second root
 disp("Using eigenvalue x2 = 3:")
 #   make B matrix by replacing x with 3 in A - x*I
@@ -50,8 +46,6 @@ syms k1 k2;
 k1 = solve(-2*k1+2*k2-0)
 #   let k1 = 1, then k2 =1
 Eigenvector2 = [1;1]
-
-
 #   A matrix created by the last digit from my registration number, 0
 A = [1, 0; 0, 1]
 #   symbolic var x
@@ -66,7 +60,6 @@ detAsubxI = det(AsubxI)
 eigenvalues = double(solve(detAsubxI == 0))
 #   one root
 x1 = 1;
-
 #   use root
 disp("Using eigenvalue x = 1:")
 #   make B matrix by replacing x with 1 in A - x*I
@@ -76,6 +69,6 @@ B
 #   symbolic var k1, k2
 syms k1 k2;
 #   solve equation to find k1, k2
-k1 = solve(2*k1+2*k2-0)
+k1 = solve(0*k1+0*k2-0)
 #   let k1 = 1, then k2 = -1
 Eigenvector1 = [1;-1]
