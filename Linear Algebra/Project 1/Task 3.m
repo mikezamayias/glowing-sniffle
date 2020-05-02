8de6dcd634d43e50cf6865c78c90f29915ecdf1f
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-05-02 05:41:16 +0300

    minor graphs tweak

--- Linear Algebra/Project 1/Task 3.m
+++ Linear Algebra/Project 1/Task 3.m
@@ -21,9 +21,9 @@ ellipse = [xc + 2*yc, 2*xc + yc];
 
 fig1 = figure();
 hold on
-plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410], ";Circle;");
+plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410]);
 hold on
-plot(ellipse(:,1), ellipse(:,2), "linewidth", 2, "color", [0.9290 0.6940 0.1250], ";Ellipse;");
+plot(ellipse(:,1), ellipse(:,2), "linewidth", 2, "color", [0.9290 0.6940 0.1250]);
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
@@ -35,9 +35,9 @@ ellipse = [xc, yc];
 
 fig2 = figure();
 hold on
-plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410], ";Circle;");
+plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410]);
 hold on
-plot(ellipse(:,1), ellipse(:,2), "linewidth", 2, "color", [0.9290 0.6940 0.1250], ";Ellipse;");
+plot(ellipse(:,1), ellipse(:,2), "linewidth", 2, "color", [0.9290 0.6940 0.1250]);
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
