6925ba6f657b03561dbef3bf7fc1cde2627ff0dc
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-05-02 19:13:16 +0300

    small changes in task 3 to help with answer in tex

--- Linear Algebra/Project 1/Task 3.m
+++ Linear Algebra/Project 1/Task 3.m
@@ -10,11 +10,13 @@ xc = cos(t);
 yc = sin(t);
 circle = [xc, yc];
 syms x y;
-B = [x; y]
+temp_matrix = [x; y]
 A = [1, 2; 2, 1]
-A*B
+A*temp_matrix
 ellipse = [xc + 2*yc, 2*xc + yc];
-fig1 = figure();
+%   fig1 = figure();
+fig = figure();
+subplot(1, 2, 1);
 hold on
 plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410]);
 hold on
@@ -22,11 +24,13 @@ plot(ellipse(:,1), ellipse(:,2), "linewidth", 2, "color", [0.6350 0.0780 0.1840]
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
-print(fig1,'-dpngalpha', '-r300', 'a.png');
+title("Figure 3 I");
+%   print(fig1,'-dpngalpha', '-r300', 'a.png');
 A = [1, 0; 0, 1]
-A*B
+A*temp_matrix
 ellipse = [xc, yc];
-fig2 = figure();
+%   fig2 = figure();
+subplot(1, 2, 2);
 hold on
 plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410]);
 hold on
@@ -34,4 +38,6 @@ plot(ellipse(:,1), ellipse(:,2), "linewidth", 2, "color", [0.6350 0.0780 0.1840]
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
-print(fig2,'-dpngalpha', '-r300', 'i.png');
+title("Figure 3 II");
+%   print(fig2,'-dpngalpha', '-r300', 'i.png');
+print(fig,'-dpngalpha', '-r300', '3i_ii.png');
