854efd2d6d983f55e95aaae3e77a3edcf3ff7d13
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-05-02 18:58:28 +0300

    small tweaks to help with answer in project tex

--- Linear Algebra/Project 1/Task 4.m
+++ Linear Algebra/Project 1/Task 4.m
@@ -9,11 +9,14 @@ xc = cos(t);
 yc = sin(t);
 circle = [xc, yc];
 ellipse = [xc + 2*yc, 2*xc + yc];
-eigvec_i1 = abs(-1)*[1; -1];
-eigvec_i2 = abs(3)*[1; 1];
-eigvec_ii1 = 1*[1; 0];
-eigvec_ii2 = 1*[0; 1];
-fig1 = figure();
+eigvec_i1 = abs(-1)*[1; -1]
+eigvec_i2 = abs(3)*[1; 1]
+eigvec_ii1 = 1*[1; 0]
+eigvec_ii2 = 1*[0; 1]
+exit
+%   fig1 = figure();
+fig = figure();
+subplot(1, 2, 1);
 hold on
 plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410]);
 hold on
@@ -25,9 +28,11 @@ quiver(0, 0, eigvec_i2(1), eigvec_i2(2), "linewidth", 2, "color", [0.6350 0.0780
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
-print(fig1,'-dpngalpha', '-r300', '4i.png');
+title("Figure 4 I");
+%   print(fig1,'-dpngalpha', '-r300', '4i.png');
 ellipse = [xc, yc];
-fig2 = figure();
+%   fig2 = figure();
+subplot(1, 2, 2);
 hold on
 plot(circle(:,1), circle(:,2), "linewidth", 2, "color", [0 0.4470 0.7410]);
 hold on
@@ -39,4 +44,6 @@ quiver(0, 0, eigvec_ii2(1), eigvec_ii2(2), "linewidth", 2, "color", [0.6350 0.07
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
-print(fig2,'-dpngalpha', '-r300', '4ii.png');
+title("Figure 4 II");
+%   print(fig2,'-dpngalpha', '-r300', '4ii.png');
+print(fig,'-dpngalpha', '-r300', '4i_ii.png');
