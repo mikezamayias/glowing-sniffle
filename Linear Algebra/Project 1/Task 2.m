beb23b17673a896af58bfdcefc4950de914234e3
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-05-02 19:58:54 +0300

    minor changes to make things easier in tex

--- Linear Algebra/Project 1/Task 2.m
+++ Linear Algebra/Project 1/Task 2.m
@@ -9,9 +9,9 @@ x1 = [1; 0]
 x2 = [0; 1]
 Ax1 = A*x1
 Ax2 = A*x2
-#fig = figure();
-fig1 = figure();
-#   subplot(1, 2, 1);
+fig = figure();
+%   fig1 = figure();
+subplot(1, 2, 1);
 hold on
 #   plot([0, x1(1)], [0, x1(2)], "linewidth", 2, "color", [0 0.4470 0.7410], ";x1;")
 #   plot(0, 0, "color", [0 0.4470 0.7410], ";x1;")
@@ -23,9 +23,10 @@ quiver(0, 0, Ax1(1), Ax1(2), "linewidth", 2, "color", [0.6350 0.0780 0.1840])
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
-print(fig1,'-dpngalpha', '-r300', 'x1.png');
-fig2 = figure();
-#   subplot(1, 2, 2);
+title("Figure 2 I");
+%   print(fig1,'-dpngalpha', '-r300', 'x1.png');
+%   fig2 = figure();
+subplot(1, 2, 2);
 hold on
 #   plot([0, x2(1)], [0, x2(2)], "linewidth", 2, "color", [0 0.4470 0.7410], ";x2;")
 #   plot(0, 0, "color", [0 0.4470 0.7410], ";x2;")
@@ -37,5 +38,6 @@ quiver(0, 0, Ax2(1), Ax2(2), "linewidth", 2, "color", [0.6350 0.0780 0.1840])
 grid on;
 box on;
 axis([axis_low, axis_high, axis_low, axis_high], "square");
-print(fig2,'-dpngalpha', '-r300', 'x2.png');
-#print(fig,'-dpngalpha', '-r300', 'x.png');
+title("Figure 2 II");
+%   print(fig2,'-dpngalpha', '-r300', 'x2.png');
+print(fig,'-dpngalpha', '-r300', '2i_ii.png');
