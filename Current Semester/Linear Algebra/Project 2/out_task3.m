8eea64ca48f77f0ae8bc84d7abf3e5fcc6a49703
mike-zamayias <mzamagias@gmail.com>
2020-05-26 12:41:31 +0300

    renamed task3

--- Current Semester/Linear Algebra/Project 2/out_task3.m
+++ Current Semester/Linear Algebra/Project 2/out_task3.m
@@ -8,9 +8,7 @@ v = [2, -3, 1];
 w = [1, -1, 0];
 #   matrix A from vectors
 A = [u; v; w];
-#   transpose A
-A = A';
 #   make zero column matrix b the size of A
 b = zeros(size(A)(1), 1);
 #   run first task's logic
-[u, v, w] = function_task1(A, b)
+[l1, l2, l3] = function_task1(A, b)
