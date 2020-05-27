1c336ff686c46dcb113893e42ee9d0f4e3a4aa72
mike-zamayias <mzamagias@gmail.com>
2020-05-27 19:37:55 +0300

    updated task 4

--- Current Semester/Linear Algebra/Project 2/task4.m
+++ Current Semester/Linear Algebra/Project 2/task4.m
@@ -1,9 +1,15 @@
 clc;
 clear all;
 close all;
-
 #   vectors u, v, w
 u = [1, -2, 3];
 v = [2, -1, 2];
 w = [1, -2, 1];
-
+#   check if vector set is basis
+basis = lin_dep(u, v, w);
+if (basis == 1)
+    disp("Orthonormal basis:");
+    [e1, e2, e3] = orthonorm_base(u, v, w);
+else
+    disp("No basis");
+endif
