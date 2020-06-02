fec5507d3b2eea02cf00a805e25a8e3700326337
mike-zamayias <mzamagias@gmail.com>
2020-06-02 17:28:36 +0300

    added a new file to run all project's octave scripts automatically

---
+++ Current Semester/Linear Algebra/Project 2/run_octave_scripts.py
@@ -0,0 +1,10 @@
+from os import system
+scripts = [
+    "rm task1.txt && octave task1.m >> task1.txt",
+    "rm task2.txt && octave task2.m >> task2.txt",
+    "rm task3.txt && octave task3.m >> task3.txt",
+    "rm task4.txt && octave task4.m >> task4.txt",
+    "rm out_task3.txt && octave out_task3.m >> out_task3.txt"
+]
+for script in scripts:
+    system(script)
