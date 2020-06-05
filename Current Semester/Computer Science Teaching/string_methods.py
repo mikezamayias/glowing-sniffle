3ca84fc1211f4a3eccf726993b42ecb3db555b3d
mike-zamayias <mzamagias@gmail.com>
2020-06-05 13:03:13 +0300

    updated script

--- Current Semester/Computer Science Teaching/string_methods.py
+++ Current Semester/Computer Science Teaching/string_methods.py
@@ -30,10 +30,10 @@ e_index = name.find('e')
 print(f'Index of "e": {e_index}')
 
 #   check if name contains just characters
-print(f'{name} containts just characters: {name.isalpha()}')
+print(f'"{name}" containts just characters: {name.isalpha()}')
 
 #   check if name containts characters or numbers
-print(f'{name} containts characters or numbers: {name.isalnum()}')
+print(f'"{name}" containts characters or numbers: {name.isalnum()}')
 
 #   check if name containts just numbers
-print(f'{name} containts characters or numbers: {name.isnumeric()}')
+print(f'"{name}" containts characters or numbers: {name.isnumeric()}')
