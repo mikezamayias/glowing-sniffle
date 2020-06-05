1109dd55c829e66f6a43b101df023e9d5666aa71
mike-zamayias <mzamagias@gmail.com>
2020-06-05 07:30:45 +0300

    added a .py file to use in the project

---
+++ Current Semester/Computer Science Teaching/strings.py
@@ -0,0 +1,13 @@
+name = 'Mike'
+age = 20
+
+#   print(name)
+#   print(age)
+
+print('Hello, what\'s your name?')
+print('How old are you?')
+
+greetings_reply = f'Greetings, my name is {name}!'
+age_reply = f'I am {age} years old.'
+reply = f'{greetings_reply} {age_reply}'
+print(reply)
