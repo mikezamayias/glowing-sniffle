fe91900008918ef402480fce33f7f0e89440ab95
Mike Zamayias <mzamayias@pop-os.localdomain>
2020-06-16 02:24:30 +0300

    updated task 1, project 3, linear algebra

--- Current Semester/Linear Algebra/Project 3/task1.m
+++ Current Semester/Linear Algebra/Project 3/task1.m
@@ -8,7 +8,34 @@ A = [1, 2, 3; 2, 4, 5; 3, 5, 0];
 [rws, cols] = size(A);
 #   if A square matrix, then do stuff
 if (rws == cols)
-    task1_function(A)
+    disp('A =')
+    disp(A);
+    if (det(A) != 0)
+        disp('Determinant of A does not equal to 0.')
+    else
+        disp('Determinant of A equals to 0.')
+        [unit_eigenvectors, eigenvalues, characteristic_polynomial] = task1_function(A)
+    endif
+else
+    disp('A is not square matrix.')
+endif
+
+disp("\n");
+
+#   matrix A
+A = [1, 2, 3; 4, 5, 6; 7, 8, 9];
+
+[rws, cols] = size(A);
+#   if A square matrix, then do stuff
+if (rws == cols)
+    disp('A =')
+    disp(A);
+    if (det(A) != 0)
+        disp('Determinant of A does not equal to 0.')
+    else
+        disp('Determinant of A equals to 0.')
+        [unit_eigenvectors, eigenvalues, characteristic_polynomial] = task1_function(A)
+    endif
 else
     disp('A is not square matrix.')
 endif
--- Current Semester/Linear Algebra/Project 3/task1_function.m
+++ Current Semester/Linear Algebra/Project 3/task1_function.m
@@ -1,8 +1,42 @@
-function task1_function(A)
-    A
-    characteristic_polynomial = polyout(poly(A), 'x')
-    [eve, eva] = eig(A, balanceOption='vector')
-    eva1 = eva(1)
-    eva2 = eva(2)
-    eva3 = eva(3)
+function [eves, evas, characteristic_polynomial] = task1_function(A)
+    #   eigenvectors and eigenvalues at eves and evas respectively
+    [eves, evas] = eig(A, balanceOption='vector');
+    #   convert eigenvector matrix to a single column
+    eves = eves(: );
+    #   make each unit eigenvector
+    eve1 = eves(1: 3)/norm(eves(1: 3));
+    eve2 = eves(4: 6)/norm(eves(4: 6));
+    eve3 = eves(7: 9)/norm(eves(7: 9));
+    #   make unit eigenvector to return
+    eves = [eve1, eve2, eve3];
+    #   tolerance, to be used for converting -0 to 0
+    tolerance = 1.e-6;
+    #   transpose evas
+    evas = evas'
+    #   calculate vieta theorem with current state evas
+    v_trace = round(sum(evas));
+    v_det = round(prod(evas));
+    #   multiply a 3x3 identity matrix with eigenvalues
+    evas = eye(3)*evas';
+    #   calculate vieta theorem with altered evas
+    check_v_trace = round(sum(evas));
+    check_v_det = round(prod(evas));
+    #   transpose evas to row
+    evas = evas';
+    #   convert -0 to 0
+    evas(evas<0 & evas>-tolerance) = 0;
+    #   round evas
+    evas = round(evas);
+    #   set coefficients of characteristc polynomial of A to pre_polynomial
+    pre_polynomial = poly(A);
+    #   convert any -0 to 0
+    pre_polynomial(pre_polynomial<0 & pre_polynomial>-tolerance) = 0;
+    #   match coeffcients with variable x at characteristic_polynomial
+    characteristic_polynomial = polyout(round(pre_polynomial), 'x');
+    #   output message for vieta's formulae
+    if (v_trace == check_v_trace & v_det == check_v_det)
+        disp("Vieta's formulae are valid for matrix A")
+    else
+        disp("Vieta's formulae are not valid for matrix A")
+    endif
 endfunction
