1b179e30b640ad37def8daa6ee13b3f3e08d7142
Mike Zamayias <mzamagias@gmail.com>
2020-09-18 21:44:55 +0300

    Programming, Lab 06: Task 2 of 3 finished.

---
+++ Current Courses/Programming/Lab/06/08.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAXIMUM_NUMBER +100
+#define MINIMUM_NUMBER -100
+#define ARRAY_SIZE 10
+
+int main()
+{
+    srand(time(NULL));
+
+    int mat[ARRAY_SIZE], i, sum = 0, counter = 0;
+
+    for (i = 0; i < ARRAY_SIZE; i++)
+    {
+        mat[i] = rand() % (MAXIMUM_NUMBER + 1 - MINIMUM_NUMBER) + MINIMUM_NUMBER;
+        sum += mat[i];
+        if (mat[i] > 0)
+        {
+            counter++;
+        }
+    }
+
+    printf("%i ακέραιοι, από το %i έως το %i:\n", ARRAY_SIZE, MINIMUM_NUMBER, MAXIMUM_NUMBER);
+    for (i = 0; i < ARRAY_SIZE; i++)
+    {
+        printf("%i\t", mat[i]);
+    }
+    printf("\n");
+    printf("Το άθροισμά των παραπάνω ακεραίων:\n%i\n", sum);
+    printf("Το πλήθος των θετικών ακεραίων:\n%i\n", counter);
+
+    return 0;
+}
