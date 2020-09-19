f1fec99a3d4f98677848ef4add6fb0b9d00884ca
Mike Zamayias <mzamagias@gmail.com>
2020-09-19 13:09:52 +0300

    Programming, Lab 08: minor update for task 08.c

--- Current Courses/Programming/Lab/08/08.c
+++ Current Courses/Programming/Lab/08/08.c
@@ -13,9 +13,6 @@ int main()
     printf("Δώσε δευετερόλεπτα:\n");
     scanf("%i", &ss);
 
-    // how many seconds in 4 hours, 3 minutes, 51 seconds
-    // 14631 seconds
-
     printf("Οι %2i ώρες, %2i λεπτά και %2i δευτερόλεπτα είναι %i δευτερόλεπτα.\n", hs, ms, ss, seconds(hs, ms, ss));
 
     return 0;
@@ -23,10 +20,5 @@ int main()
 
 int seconds(int hs, int ms, int ss)
 {
-    // int seconds;
-    // seconds = ss;
-    // seconds += 60 * ms;
-    // seconds += 60 * 60 * hs;
-    // return seconds;
     return hs * 60 * 60 + ms * 60 + ss;
 }
