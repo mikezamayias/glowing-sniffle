09d83b3949d44afa61500c6bf97d3ce82ad70427
Mike Zamayias <mzamagias@gmail.com>
2020-10-12 09:27:41 +0300

    fixed programs logic, now (i guess) it works correctly

--- Current Courses/Plain Driven Programming/Lab/Assignment 3/human.py
+++ Current Courses/Plain Driven Programming/Lab/Assignment 3/human.py
@@ -57,15 +57,16 @@ class HeartsPlayer(Human):
         return self.points
 
     def print_hand(self):
+        print(f"{self.nickname}'s hand:")
         for card in self.hand:
-            print(card.get_card())
+            print(card.pretty_card())
 
 
 class HeartsDealer(Human):
     deck = Deck()
     deck.create_deck()
     deck.shuffle_deck()
-    cards_to_deal = 2
+    cards_to_deal = 5
     round_counter = 0
 
     def __init__(self, first_name, last_name, age):
@@ -85,46 +86,31 @@ class HeartsDealer(Human):
             for player in [player1, player2]:
                 player.set_hand(self.deal_random_card())
 
-    def decide_winner(self, player1: HeartsPlayer, player2: HeartsPlayer):
-        player1s_hearts = 0
-        player2s_hearts = 0
-        self.cards_to_deal = 1
-        while True:
-            if self.round_counter == 2:
-                print(f"{player1.nickname}'s hand:")
-                for card in player1.get_hand():
-                    print(card.pretty_card())
-                print(f"{player2.nickname}'s hand:")
-                for card in player2.get_hand():
-                    print(card.pretty_card())
-                if player1.get_points() > player2.get_points():
-                    winner = player1
-                    losser = player2
-                    print(
-                        f"{winner.get_nickname()} won {losser.get_nickname()} with {winner.get_points()} to {losser.get_points()}.")
-                elif player2.get_points() > player1.get_points():
-                    winner = player2
-                    losser = player1
-                    print(
-                        f"{winner.get_nickname()} won {losser.get_nickname()} with {winner.get_points()} to {losser.get_points()}.")
-                else:
-                    print(
-                        f"Both {player1.get_nickname()} and {player2.get_nickname()} have equal amount points, {player1.get_points()}.")
-                break
-            else:
-                for card in player1.get_hand():
-                    if card.is_heart():
-                        player1s_hearts += 1
-                for card in player2.get_hand():
-                    if card.is_heart():
-                        player2s_hearts += 1
+    def count_player_hearts(self, player: HeartsPlayer) -> int:
+        heart_counter = 0
+        for card in player.get_hand():
+            if card.is_heart():
+                heart_counter += 1
+        return heart_counter
 
-                diff = player1s_hearts - player2s_hearts
-                if diff > 0:
-                    player1.set_points(diff * 10)
-                elif diff < 0:
-                    player2.set_points(-diff * 10)
-                else:
-                    pass
-                self.deal_to_players(player1, player2)
-                self.round_counter += 1
+    def count_players_points(self, player1: HeartsPlayer, player2: HeartsPlayer):
+        player1_hearts = self.count_player_hearts(player1)
+        player2_hearts = self.count_player_hearts(player2)
+        hearts_difference = player1_hearts - player2_hearts
+        if hearts_difference > 0:
+            player1.set_points(hearts_difference * 10)
+        elif hearts_difference < 0:
+            player2.set_points(-1 * hearts_difference * 10)
+        else:
+            pass
+
+    def decide_winner(self, player1: HeartsPlayer, player2: HeartsPlayer):
+        if player1.get_points() > player2.get_points():
+            print(
+                f"{player1.get_nickname()} wins {player2.get_nickname()}, {player1.get_points()} to {player2.get_points()}")
+        elif player2.get_points() > player1.get_points():
+            print(
+                f"{player2.get_nickname()} wins {player1.get_nickname()}, {player2.get_points()} to {player1.get_points()}")
+        else:
+            print(
+                f"Both {player1.get_nickname()} and {player2.get_nickname()} have the same amount of Hearts, no winner.")
--- Current Courses/Plain Driven Programming/Lab/Assignment 3/main.py
+++ Current Courses/Plain Driven Programming/Lab/Assignment 3/main.py
@@ -5,14 +5,20 @@ def main():
     dealer = HeartsDealer("Nick", "Fury", 70)
     player_1 = HeartsPlayer("Tony", "Stark", 50)
     player_2 = HeartsPlayer("Steve", "Rogers", 102)
- 
-    player_1.set_nickname("Iron Man")    
+
+    player_1.set_nickname("Iron Man")
     player_2.set_nickname("Captain America")
 
-    # dealer.show_deck()
-
+    round_counter = 0
     dealer.deal_to_players(player_1, player_2)
+    dealer.cards_to_deal = 3
+    while round_counter < 5:
+        dealer.count_players_points(player_1, player_2)
+        round_counter += 1
+        dealer.deal_to_players(player_1, player_2)
 
+    player_1.print_hand()
+    player_2.print_hand()
     dealer.decide_winner(player_1, player_2)
 
 
