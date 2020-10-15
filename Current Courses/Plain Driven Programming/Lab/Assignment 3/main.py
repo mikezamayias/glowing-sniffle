05500a69c4b696894e21a19db55397f3a21f52bb
Mike Zamayias <mzamagias@gmail.com>
2020-10-15 08:20:47 +0300

    updated main

--- Current Courses/Plain Driven Programming/Lab/Assignment 3/main.py
+++ Current Courses/Plain Driven Programming/Lab/Assignment 3/main.py
@@ -1,21 +1,21 @@
-from human import HeartsPlayer, HeartsDealer
+from dealer import HeartsDealer
+from player import HeartsPlayer
 
 
 def main():
     dealer = HeartsDealer("Nick", "Fury", 70)
-    player_1 = HeartsPlayer("Tony", "Stark", 50)
-    player_2 = HeartsPlayer("Steve", "Rogers", 102)
 
+    player_1 = HeartsPlayer("Tony", "Stark", 50)
     player_1.set_nickname("Iron Man")
+
+    player_2 = HeartsPlayer("Steve", "Rogers", 102)
     player_2.set_nickname("Captain America")
 
     round_counter = 0
-    dealer.deal_to_players(player_1, player_2)
-    dealer.cards_to_deal = 3
     while round_counter < 5:
-        dealer.count_players_points(player_1, player_2)
-        round_counter += 1
         dealer.deal_to_players(player_1, player_2)
+        dealer.count_player_points(player_1, player_2)
+        round_counter += 1
 
     player_1.print_hand()
     player_2.print_hand()
