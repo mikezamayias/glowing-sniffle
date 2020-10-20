c9105590a3f331646e8bed199af61539e08a14c5
Mike Zamayias <mzamagias@gmail.com>
2020-10-20 23:59:15 +0300

    introduced some breathing space between lines

--- Current Courses/Logic Programming/Assignment 1/Task 3.pl
+++ Current Courses/Logic Programming/Assignment 1/Task 3.pl
@@ -13,27 +13,33 @@ sex_female(anna).
 
 
 % figure 3
+
 % 1.
 wife(Y, X) :-
     husband(X, Y).
+
 % 2.
 % child(Y, X) :-
     % father(X, Y).
+
 % 3.
 mother(Z, Y) :-
     father(X, Y),
     wife(Z, X).
+
 % 4. - merged rule number 2 with rule number 4
 child(Y, X) :-
     (   father(X, Y)
     ;   mother(X, Y)
     ).
+
 % 5.
 % brother(Y, Z) :-
     % father(X, Y),
     % father(X, Z),
     % sex_male(Y),
     % not(Y=Z).
+
 % 6. - merged rules number 5 and 6
 brother(Y, Z) :-
     (   father(X, Y)
@@ -44,7 +50,9 @@ brother(Y, Z) :-
     ),
     sex_male(Y),
     not(Y=Z).
+
 % 7. - rule 7 implemented in rule 8, skipped
+
 % 8. - created based on the template from rules number 5 and 6
 sister(Y, Z) :-
     (   father(X, Y)
