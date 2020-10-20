0d797c408d278733cc23e7693c2a53bd48cad528
Mike Zamayias <mzamagias@gmail.com>
2020-10-20 23:54:22 +0300

    added task 3 of assignment 1 in logic programming

---
+++ Current Courses/Logic Programming/Assignment 1/Task 3 source.pl
@@ -0,0 +1,5 @@
+% instructions
+father(x, y).
+husband(x, y).
+sex_male(x).
+sex_female(x).
---
+++ Current Courses/Logic Programming/Assignment 1/Task 3.pl
@@ -0,0 +1,57 @@
+% consult source file
+consult('Task 3 source.pl').
+
+
+% figure 2
+father(kostas, anna).
+father(kostas, aris).
+husband(kostas, soula).
+sex_male(kostas).
+sex_male(aris).
+sex_female(soula).
+sex_female(anna).
+
+
+% figoure 3
+% 1.
+wife(Y, X) :-
+    husband(X, Y).
+% 2.
+% child(Y, X) :-
+    % father(X, Y).
+% 3.
+mother(Z, Y) :-
+    father(X, Y),
+    wife(Z, X).
+% 4. - merged rule number 2 with rule number 4
+child(Y, X) :-
+    (   father(X, Y)
+    ;   mother(X, Y)
+    ).
+% 5.
+% brother(Y, Z) :-
+    % father(X, Y),
+    % father(X, Z),
+    % sex_male(Y),
+    % not(Y=Z).
+% 6. - merged rules number 5 and 6
+brother(Y, Z) :-
+    (   father(X, Y)
+    ;   mother(X, Y)
+    ),
+    (   father(X, Y)
+    ;   mother(X, Z)
+    ),
+    sex_male(Y),
+    not(Y=Z).
+% 7. - rule 7 implemented in rule 8, skipped
+% 8. - created based on the template from rules number 5 and 6
+sister(Y, Z) :-
+    (   father(X, Y)
+    ;   mother(X, Y)
+    ),
+    (   father(X, Y)
+    ;   mother(X, Z)
+    ),
+    sex_female(Y),
+    not(Y=Z).
