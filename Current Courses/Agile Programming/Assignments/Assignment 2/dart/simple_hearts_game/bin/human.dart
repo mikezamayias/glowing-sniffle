21c44e750a78349dfc038b8b76ebf22d81edd852
Mike Zamayias <mzamagias@gmail.com>
2020-12-15 03:17:15 +0200

    refactored human.dart and added human abstract class in diagram

--- Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game/bin/human.dart
+++ Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game/bin/human.dart
@@ -11,15 +11,9 @@ abstract class Human {
 
   String get firstName => _firstName;
 
-  set firstName(String value) => _firstName = value;
-
   String get lastName => _lastName;
 
-  set lastName(String value) => _lastName = value;
-
   int get age => _age;
-
-  set age(int value) => _age = value;
 
   @override
   String toString() {
