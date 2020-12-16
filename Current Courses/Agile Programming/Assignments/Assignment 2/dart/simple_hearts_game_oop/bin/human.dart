12f7e1485c18b4f1092c01dff01fa788607c8f1d
Mike Zamayias <mzamagias@gmail.com>
2020-12-16 09:57:20 +0200

    documented

--- Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/human.dart
+++ Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/human.dart
@@ -1,25 +1,34 @@
-abstract class Human {
-  final String _firstName;
-  final String _lastName;
-  final int _age;
+/// An abstract class to represent a human, which has:
+/// attributes for human's first and last name and age,
+/// attribute getters,
+/// methods.
+abstract class Constrictor {
+  final String _firstName; // Attribute
+  final String _lastName; // Attribute
+  final int _age; // Attribute
 
-  Human(
+  Constrictor(
     this._firstName,
     this._lastName,
     this._age,
-  );
+  ); // Constructor
 
+  // Getter for this human's first name.
   String get firstName => _firstName;
 
+  /// Getter for this human's last name.
   String get lastName => _lastName;
 
+  /// Getter for this human's age.
   int get age => _age;
 
+  /// Returns a formmated string containing classes attributes.
   @override
   String toString() {
     return 'Introducing: $_firstName $_lastName, $_age';
   }
 
+  /// Introduces human.
   void introduceSelf() {
     print(toString());
   }
