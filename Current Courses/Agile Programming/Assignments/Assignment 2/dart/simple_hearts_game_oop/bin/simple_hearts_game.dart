8ae7abac2edaf5a2720990be4e19c3fac87ac3ff
Mike Zamayias <mzamagias@gmail.com>
2020-12-16 10:01:11 +0200

    documented

--- Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/simple_hearts_game.dart
+++ Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/simple_hearts_game.dart
@@ -2,12 +2,14 @@ import 'hearts_dealer.dart';
 import 'hearts_player.dart';
 
 void main(List<String> args) {
+  /// Creates a hearts dealer object.
   var dealer = HeartsDealer(
     'Nick',
     'Fury',
     66,
   );
 
+  /// Creates a hearts player object.
   var player1 = HeartsPlayer(
     'Captain',
     'America',
@@ -15,6 +17,7 @@ void main(List<String> args) {
     'Steve Rogers',
   );
 
+  /// Creates another hearts player object.
   var player2 = HeartsPlayer(
     'Iron',
     'Man',
@@ -22,12 +25,15 @@ void main(List<String> args) {
     'Tony Stark',
   );
 
+  /// Dealer shuffles deck.
   dealer.shuffleDeck();
 
+  /// Play five rounds.
   for (var i = 0; i < 5; i++) {
     dealer.dealToPlayers(player1, player2);
     dealer.decideRoundWinner(player1, player2, i);
   }
 
+  /// Decide game winner.
   dealer.decideWinner(player1, player2);
 }
