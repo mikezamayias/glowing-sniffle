85abe2de57bf138e284a984d973926cd0d7ebfbe
Mike Zamayias <mzamagias@gmail.com>
2020-12-16 08:14:02 +0200

    documented

--- Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/hand.dart
+++ Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/hand.dart
@@ -1,15 +1,25 @@
 import 'playing_card.dart';
 
+/// A class to represent player's hand.
+///
+/// Player's hand has a list of playing cards attribute.
+///
+/// It also has a getter, for the attribute and two 
+/// methods, addPlayingCard to add a playing card to the
+/// hand and handSize to get the hand size.
 class Hand {
-  final List<PlayingCard> _playingCards = [];
+  final List<PlayingCard> _playingCards = []; // Attribute
 
-  Hand();
+  Hand(); // Constructor
 
+  /// Gets hand's playing cards.
   List<PlayingCard> get playingCards => _playingCards;
 
+  /// Adds card to hand.
   void addPlayingCard(PlayingCard playingCard) {
     _playingCards.add(playingCard);
   }
 
+  /// Returns hand's size.
   int handSize() => _playingCards.length;
 }
