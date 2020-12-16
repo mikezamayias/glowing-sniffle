f801a96d883e8a443d829c60bcd412990dedf565
Mike Zamayias <mzamagias@gmail.com>
2020-12-16 09:59:24 +0200

    applied some good practises

--- Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/hearts_player.dart
+++ Current Courses/Agile Programming/Assignments/Assignment 2/dart/simple_hearts_game_oop/bin/hearts_player.dart
@@ -40,30 +40,30 @@ class HeartsPlayer extends Human implements CardsPlayer {
   /// Getter for this hand.
   Hand get hand => _hand;
 
+  /// Counts Hearts in last round, or in the last 5 cards.
   int countHeartsLastRound() {
-    /// Counts Hearts in last round, or in the last 5 cards.
     return hand.playingCards
         .sublist(hand.handSize() - 5, hand.handSize())
         .where((card) => card.playingCardSuit == 'Hearts')
         .length;
   }
 
+  /// Counts Hearts in this hand.
   int countHearts() {
-    /// Counts Hearts in this hand.
     return hand.playingCards
         .where((card) => card.playingCardSuit == 'Hearts')
         .length;
   }
 
+  /// Introduces this player.
   @override
   void introduceSelf() {
-    /// Introduces this player.
     print(super.toString() + ', "$userName"');
   }
 
+  /// Shows this player's hand.
   @override
   void showHand() {
-    /// Shows this player's hand.
     print(userName);
     for (var playingCard in hand.playingCards) {
       print(playingCard.toString());
